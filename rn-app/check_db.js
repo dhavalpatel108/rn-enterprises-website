8a70dc1c00eac8e86e33bd7b70f74c186760cb7b
dhavalpatel108 <dhavalpatel108@users.noreply.github.com>
2026-08-15 13:45:34 +0530

    chore: Remove confusing local scratch files

--- rn-app/check_db.js
+++
@@ -1,9 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-
-async function main() {
-  const configs = await prisma.config.findMany();
-  console.log(JSON.stringify(configs, null, 2));
-}
-
-main().catch(console.error).finally(() => prisma.$disconnect());
--- rn-app/scratch_db_size.js
+++
@@ -1,7 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-async function main() {
-    const result = await prisma.$queryRaw`SELECT pg_size_pretty(pg_database_size(current_database())) as size`;
-    console.log(result);
-}
-main().catch(console.error).finally(() => prisma.$disconnect());
